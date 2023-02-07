13f7031e117d065670b6a046bb2d058cf66ceadc
Julia Ramos <julia.jrra@gmail.com>
2023-02-07 18:46:51 -0300

    Project section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {NavBar} from './components/NavBar';
 import {Banner} from './components/Banner';
 import {Skills} from './components/Skills';
+import {Projects} from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
