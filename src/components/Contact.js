cba131565de2cff29786dd8d23bc660d5f9880a3
Julia Ramos <julia.jrra@gmail.com>
2023-02-11 17:29:51 -0300

    Alterações em Contact e add do Server

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,8 +22,12 @@ export const Contact = () => {
         })
     }
 
-    const handleSubmit = () => {
-
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setButtonText('Sending...');
+        let response = await fetch ("http://localhost:5000/contact", {
+            /* parei aqui */
+        })
     }
 
     return(
