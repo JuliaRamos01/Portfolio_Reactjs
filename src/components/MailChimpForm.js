4c432817f1ca3a1bb780377f372512814c158422
Julia Ramos <julia.jrra@gmail.com>
2023-02-12 19:20:03 -0300

    Alterações em Newsletter

--- src/components/MailChimpForm.js
+++ src/components/MailChimpForm.js
@@ -1,4 +1,5 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Newsletter } from "./Newsletter"; /* parei aqui 1:34:27 */
 
 export const MailchimpForm = () =>{
     const postUrl = `${process.env.REACT_APP.MAILCHIMP_URL}?u=${process.env.REACT_APP.MAILCHIMP_U}$id=${process.env.REACT_APP.MAILCHIMP_ID}`:
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,6 +1,14 @@
 import {Alert} from "react-bootstrap";
+import { useState } from "react";
 
 export const Newsletter = ({subscribe, status, message}) => {
+
+    const [email, setEmail] = useState('');
+    const handleSubmit = () =>{
+
+    }
+
+
     return(
         <Col lg={12}>
             <div className="newsletter-bx">
@@ -8,8 +16,14 @@ export const Newsletter = ({subscribe, status, message}) => {
                     <Col lg={12} md={6} xl={5}>
                         <h3>Subscribe to our Newsletter</h3>
                         {status === 'sending' && <Alert>Sending...</Alert>}
-                        {status === 'error' && <Alert variant="danger">{message}</Alert>} {/* parei aqui 1:30:03 */}
-                        {status === 'sending' && <Alert>Sending...</Alert>}
+                        {status === 'error' && <Alert variant="danger">{message}</Alert>} 
+                        {status === 'success' && <Alert variant="success">{message}</Alert>}
+                    </Col>
+                    <Col md={6} xl={7}>
+                        <form onSubmit={handleSubmit}></form>
+                        <div className="new-email-bx">
+                            <imput value={} />
+                        </div>
                     </Col>
                 </Row>
             </div>
